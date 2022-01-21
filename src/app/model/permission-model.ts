b952e51c7d92dc12fdce60fc26124efb950bd3fe
Androoideka <andrej@gasic.rs>
2022-01-21 18:12:26 +0100

    Support additional permissions from backend

--- src/app/model/permission-model.ts
+++ src/app/model/permission-model.ts
@@ -1,11 +1,17 @@
-export const PERMISSION_NUMBER: number = 4;
-
 export const PERMISSION_REPRESENTATIONS: string[] = [
   'can_create_users',
   'can_read_users',
   'can_update_users',
-  'can_delete_users'
+  'can_delete_users',
+  'can_search_machines',
+  'can_start_machines',
+  'can_stop_machines',
+  'can_restart_machines',
+  'can_create_machines',
+  'can_destroy_machines'
 ]
+
+export const PERMISSION_NUMBER: number = PERMISSION_REPRESENTATIONS.length;
 
 const PERMISSION_SHORT_VALUES: string[] = [
   'c',
@@ -99,7 +105,7 @@ export class PermissionList {
 
   get shortDisplay(): string {
     let display: string = '';
-    for (let i = 0; i < this.permissionValues.length; i++) {
+    for (let i = 0; i < 4; i++) {
       if (this.permissionValues[i]) {
         display += PERMISSION_SHORT_VALUES[i];
       } else {
